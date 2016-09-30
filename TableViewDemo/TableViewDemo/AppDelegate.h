406a4d299048feb9b3dde57dac1a78ef0c615ae6
zyMeXue <zy@MeXue.com>
2016-09-30 15:58:20 +0800

    create

---
+++ TableViewDemo/TableViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TableViewDemo
+//
+//  Created by zhangyan on 16/9/30.
+//  Copyright © 2016年 zhangyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TableViewDemo/TableViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TableViewDemo
+//
+//  Created by zhangyan on 16/9/30.
+//  Copyright © 2016年 zhangyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
